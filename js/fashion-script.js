3797b257100e408172f604d4d2cb9aea1d932891
Anatolij <tolik20002@bigmir.net>
2015-08-02 22:28:04 +0300

    slider

---
+++ js/fashion-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.my-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 255000,
+        asNavFor: '.my-carousel'
+    });
+})
