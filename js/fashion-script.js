9ca91b4f06f0e20678c20a6f40d5f99d4cf874a2
Anatolij <tolik20002@bigmir.net>
2015-08-03 00:30:38 +0300

    product

--- js/fashion-script.js
+++ js/fashion-script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 255000,
+        autoplaySpeed: 5000,
         asNavFor: '.my-carousel'
     });
-})
+});
